9baf705389818a6435bfd3b554887bf6f4840d73
Ahmed_Adam <ibn.abuadam@gmail.com>
2020-07-17 02:09:11 +0200

    models

---
+++ RestContries/AppSenes/SelectedCountry/Services/Model/SelectedCountryModel.swift
@@ -0,0 +1,26 @@
+//
+//  SelectedCountryModel.swift
+//  RestContries
+//
+//  Created by Adam on 7/17/20.
+//  Copyright (c) 2020 TaskApp. All rights reserved.
+//  Modify By:  * Ahmed Adam
+//              * ibn.abuadam@gmail.com
+//              * https://github.com/AhmedibnAdam
+
+import UIKit
+
+struct SelectedCountryModel {	
+	struct Request {
+		// do someting...
+
+		func parameters() -> [String: Any]? {
+			// do someting...
+			return nil
+		}
+	}
+
+	struct Response {
+		// do someting...
+	}
+}
